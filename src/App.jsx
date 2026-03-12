4215192e0756764d66f51523bf639573ef57a22e
wlewis0991 <wlewis1990@gmail.com>
2026-03-12 11:26:38 -0700

    Seperate main pages deleted. Preview will also hold as the template that the imput sections will be loaded into, so maybe rename

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 import { useState } from 'react'
-
+import Education from './Education.jsx'
+import Pesonal from './Personal.jsx'
+import Projects from './Projects.jsx'
+import Welcome from './Welcome.jsx'
+import Work from './Work.jsx'
 
 function App() {
 
